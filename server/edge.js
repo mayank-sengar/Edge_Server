97df61a1f354ad9a29f89fd090f74328975818f5
mayank-sengar <btech10111.23@bitmesra.ac.in>
2026-03-23 23:08:54 +0530

    implementing

--- server/edge.js
+++ server/edge.js
@@ -1,10 +1,16 @@
-import express from 'express';
+import express, { response } from 'express';
 import axios from 'axios';
 import Redis from 'ioredis';
-import { use } from 'react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const redis= new Redis();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const TIME_API_URL = 'https://worldtimeapi.org/api/timezone/Etc/UTC';
+const RESPONSE_MESSAGE = 'Protected edge response';
+const PORT = process.env.PORT || 3001;
 
 
 async function rateLimit(ip){
@@ -49,13 +55,12 @@ app.get("/verify",async(req,res)=>{
     const ip = req.ip;
 
 
-    if(answer==17){
+    if(answer === "17"){
         await redis.set(`verified:${ip}`,"true","EX",600);
-        res.send("Verification success");
+        return res.redirect("/");
     }
-    else{
-        res.send("Verification failed");
-    }
+
+    return res.status(400).send("Verification failed");
 })
 
 async function isVerified(ip){
@@ -63,3 +68,63 @@ async function isVerified(ip){
     return result == "true";
 }
 
+async function getCache(key){
+    const data=await redis.get(key);
+
+    if(!data) return null;
+    return JSON.parse(data);
+}
+
+async function setCache(key,data){
+    await redis.set(key,JSON.stringify(data),"EX",30);
+    
+}
+
+//middleware
+
+app.use(async (req,res,next)=> {
+    const ip= req.ip;
+
+    if(await isBlackListed(ip)){
+        return res.status(403).send("You are blacklisted")
+    }
+
+    const allowed = await rateLimit(ip);
+    if(!allowed){
+        await blacklist(ip);
+        return res.status(429).send("Too many request. You are blacklisted for 1 hour")
+    }
+
+    if(detectBot(req)){
+        if(!await isVerified(ip)){
+        //     return res.status(403).send("Please verify you are human by visiting /verify?answer=17")
+        return res.sendFile(__dirname+ "/captcha.html");
+        }
+    }
+
+
+    next();
+
+
+})
+app.get('/', async (req, res) => {
+    const key = `response:${req.ip}`;
+
+    try {
+        const cached = await getCache(key);
+        if (cached) return res.json({ ...cached, source: 'cache' });
+
+        const response = await axios.get("http://localhost:3000/")
+       const payload=response.data;
+
+        await setCache(key, payload);
+        res.json({ ...payload, source: 'origin' });
+
+    } catch (e) {
+        res.status(502).json({ message: 'Failed', error: e.message });
+    }
+});
+
+app.listen(3001, () => {
+    console.log(`Listening on PORT: ${3001}`);
+});
